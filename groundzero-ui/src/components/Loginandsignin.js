0f244e35ae25eda6277220e4e14a3f2f7a90a6ed
saisrivenkat <50038114+saisrivenkat@users.noreply.github.com>
2020-10-15 14:49:41 +0530

    added validation

--- groundzero-ui/src/components/Loginandsignin.js
+++ groundzero-ui/src/components/Loginandsignin.js
@@ -4,7 +4,6 @@ import background from "../resources/FullLogo_Black (1).png";
 import "../login.css";
 import {Button} from "reactstrap";
 import Snackbar from "@material-ui/core/Snackbar";
-import Icon from "@material-ui/core/IconButton";
 import MuiAlert from '@material-ui/lab/Alert';
 
 function Alert(props) {
@@ -15,18 +14,41 @@ function script() {
   const sign_in_btn = document.querySelector("#sign-in-btn");
   const sign_up_btn = document.querySelector("#sign-up-btn");
   const container = document.querySelector(".container-fluid-login");
-
   sign_up_btn.addEventListener("click", () => {
     container.classList.add("sign-up-mode");
   });
-
   sign_in_btn.addEventListener("click", () => {
     container.classList.remove("sign-up-mode");
   });
 }
 
-class Auth  extends React.Component {
+const intialstate={
+    username: '',
+    userfirstname: '',
+    userlastname: '',
+    userphone: '',
+    useremail: '',
+    userpassword: '',
+    nameerror:'',
+    passworderror: '',
+    emailerror:'',
+    phoneerror:''
+}
+const intialfacultystate={
+  facultyname:'',
+  facultyfirstname:'',
+  facultylastname:'',
+  facultyemail:'',
+  facultyphone:'',
+  facultypassword:'',
+  preferredsubject:'',
+  nameerror:'',
+  passworderror: '',
+  emailerror:'',
+  phoneerror:''
+}
 
+class Auth  extends React.Component {
 
   constructor(props) {
     super(props);
@@ -45,7 +67,12 @@ class Auth  extends React.Component {
       facultyemail:'',
       facultyphone:'',
       facultypassword:'',
-      preferredsubject:''
+      preferredsubject:'',
+
+      nameerror:'',
+      passworderror:'',
+      emailerror:'',
+      phoneerror:''
     }
     this.state={
       snackbaropen:false,
@@ -59,36 +86,98 @@ class Auth  extends React.Component {
   snackbarClose = (event) => {
     this.setState({snackbaropen:false});
   }
+  validatefaculty=()=>{
+    let nameerror='';
+    let passworderror='';
+    let emailerror='';
+    let phoneerror='';
+
+    if(!this.state.facultyname)
+    {
+      nameerror="name cant be blank";
+    }
+    if(!this.state.facultyemail){
+      emailerror="email cant be blank";
+    }
+    if(!this.state.facultypassword){
+      passworderror="password cant be blank";
+    }
+    if(!this.state.facultyphone)
+    {
+      phoneerror="phone number should not be blank";
+    }
+
+    if(nameerror || emailerror || phoneerror || passworderror )
+    {
+      this.setState({nameerror,emailerror,phoneerror,passworderror});
+      return false;
+    }
+    return true;
+
+  }
+  validate =() => {
+    let nameerror='';
+    let passworderror='';
+    let emailerror='';
+    let phoneerror='';
+
+    if(!this.state.username){
+      nameerror="name cant be blank";
+    }
+    if(!this.state.useremail ){
+          emailerror="email cant be blank";
+    }
+    if(!this.state.userpassword){
+          passworderror="password cant be blank";
+    }
+    if(!this.state.userphone)
+    {
+      phoneerror="phone number should not be blank";
+    }
+
+    if(nameerror || emailerror || phoneerror || passworderror )
+    {
+     this.setState({nameerror,emailerror,phoneerror,passworderror});
+      return false;
+    }
+    return true;
+
+  }
 
   handlefacultychange = (event) =>{
     event.preventDefault();
     this.setState({
       [event.target.name]: event.target.value
     })
+
   }
   facultysubmit = (event) => {
     event.preventDefault();
+    const isvalid=this.validatefaculty();
     let data = {
       facultyName:this.state.facultyname,
       facultyEmail:this.state.facultyemail,
       facultyPhone:this.state.facultyphone,
       password:this.state.facultypassword
     }
+    if(isvalid){
+      fetch('http://groundzerolearnings-env.eba-7e4bkbxz.us-east-1.elasticbeanstalk.com/api/faculty/savefaculty', {
+        method: 'POST',
+        body: JSON.stringify(data), // data can be `string` or {object}!
 
-    fetch('http://groundzerolearnings-env.eba-7e4bkbxz.us-east-1.elasticbeanstalk.com/api/faculty/savefaculty', {
-      method: 'POST',
-      body: JSON.stringify(data), // data can be `string` or {object}!
+        headers: {'Content-Type': 'application/json'}
+      })
 
-      headers: {'Content-Type': 'application/json'}
-    })
+          .then(res => res.json())
 
-        .then(res => res.json())
+          .catch(error => console.error('Error:', error))
 
-        .catch(error => console.error('Error:', error))
+          .then(response => console.log('Success:', response));
 
-        .then(response => console.log('Success:', response));
+      this.setState({snackbaropen:true,snackbarmsg:'Stored Successfully'});
+      this.setState(intialfacultystate);
 
-    this.setState({snackbaropen:true,snackbarmsg:'Stored Successfully'});
+    }
 
   }
 
@@ -102,7 +191,7 @@ class Auth  extends React.Component {
 
   handleSubmit(event) {
     event.preventDefault();
-
+    const isvalid =this.validate();
 
     let data = {
       userFullName: this.state.username,
@@ -111,12 +200,13 @@ class Auth  extends React.Component {
       password:this.state.userpassword
     }
 
-
+    if(isvalid)
+    {
     fetch('http://groundzerolearnings-env.eba-7e4bkbxz.us-east-1.elasticbeanstalk.com/api/user/saveUser', {
-      method: 'POST',
-      body: JSON.stringify(data), // data can be `string` or {object}!
+        method: 'POST',
+        body: JSON.stringify(data), // data can be `string` or {object}!
 
-      headers: {'Content-Type': 'application/json'}
+        headers: {'Content-Type': 'application/json'}
     })
 
         .then(res => res.json())
@@ -127,6 +217,9 @@ class Auth  extends React.Component {
 
 
     this.setState({snackbaropen:true,snackbarmsg:'Stored Successfully'});
+    this.setState(intialstate);
+
+}
 
 
   }
@@ -135,8 +228,6 @@ class Auth  extends React.Component {
     return (
         <div>
           <div style={{backgroundColor:""}} className="container-fluid-login">
-
-
             <Snackbar anchorOrigin={{vertical:'top',horizontal:'right'}} open={this.state.snackbaropen} autoHideDuration={3000} onClose={this.snackbarClose} message={<span id="message-id" >{this.state.snackbarmsg}</span>} >
               <Alert onClose={this.snackbarClose} severity="success">
                 {this.state.snackbarmsg}
@@ -148,97 +239,85 @@ class Auth  extends React.Component {
                   <img style={{
                      width:"200px",
                     padding:"25px",
-
                   }} src={img} className="image  d-none d-md-none d-lg-block d-xl-block " alt=""/>
                   <h2 className="title">SIGNUP FOR STUDENTS</h2>
-
                   <div className="input-field">
                     <i className="fas fa-user"/>
-                    <input type="text" name="username" value={this.state.username} onChange={this.handleChange}
-                           placeholder="Username"/>
+                    <input type="text" name="username" value={this.state.username} onChange={this.handleChange} placeholder="Username"/>
                   </div>
-
+                    <div style={{color:"red",fontSize:"15px"}}>{this.state.nameerror}</div>
                   <div className="input-field">
                     <i className="fas fa-phone"/>
-                    <input type="number" name="userphone" value={this.state.userphone} onChange={this.handleChange}
-                           placeholder="Phone number"/>
+                    <input type="number" name="userphone" value={this.state.userphone} onChange={this.handleChange} placeholder="Phone number"/>
                   </div>
+                    <div  style={{color:"red",fontSize:"15px"}}>{this.state.phoneerror}</div>
                   <div className="input-field">
                     <i className="fas fa-envelope"/>
-                    <input type="email" name="useremail" value={this.state.useremail} onChange={this.handleChange}
-                           placeholder="Email"/>
+                    <input type="email" name="useremail" value={this.state.useremail} onChange={this.handleChange} placeholder="Email"/>
                   </div>
+                    <div  style={{color:"red",fontSize:"15px"}}>{this.state.emailerror}</div>
                   <div className="input-field">
                     <i className="fas fa-lock"/>
-                    <input  type="password" name="userpassword" value={this.state.userpassword}
-                           onChange={this.handleChange} placeholder="Password"/>
+                    <input  type="password" name="userpassword" value={this.state.userpassword} onChange={this.handleChange} placeholder="Password"/>
                   </div>
+                    <div style={{color:"red",fontSize:"15px"}}>{this.state.passworderror}</div>
                   <Button style={{width:"100%",maxWidth:"385px",borderRadius:"55px",height:"45px"}} outline color="info" onClick={this.handleSubmit} >Register</Button>
-
-                  {/*<button outline color="info"   onClick={this.handleSubmit} className="btn btn-primary submit-button">Register</button>*/}
-
-                  {/*<input onSubmit={this.handleSubmit} type="submit" value="Login" className="button solid"/>*/}
                   <p style={{marginBottom:0}} className="social-text">Or Sign in with social platforms</p>
                     <div style={{display: "flex"}} className="social">
                         <a href="https://www.facebook.com/GZLearnings/">
-                            <img src="https://img.icons8.com/color/48/000000/facebook.png"/>
+                            <img src="https://img.icons8.com/color/48/000000/facebook.png" alt="social-icon"/>
                         </a>
                         <a href="https://www.instagram.com/groundzerolearnings/" >
-                          <img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/>
+                          <img src="https://img.icons8.com/fluent/48/000000/instagram-new.png" alt="social-icon"/>
                         </a>
                         <a href="https://g.page/groundzero-learnings/review?rc">
-                            <img src="https://img.icons8.com/color/48/000000/google-plus-squared.png"/>
+                            <img src="https://img.icons8.com/color/48/000000/google-plus-squared.png" alt="social-icon"/>
                         </a>
-                        <a href="https://www.linkedin.com/company/groundzero-learnings"
-                        >
-                            <img src="https://img.icons8.com/color/48/000000/linkedin.png"/>
+                        <a href="https://www.linkedin.com/company/groundzero-learnings">
+                            <img src="https://img.icons8.com/color/48/000000/linkedin.png" alt="social-icon"/>
                         </a>
                     </div>
                 </form>
-
                 <form action="#" method="POST" className="sign-up-form">
                   <img style={{
                     width:"200px",
                     padding:"25px",
-
                   }} src={img} className="image " alt=""/>
                   <h2 className="title">SIGN UP FOR FACULTY</h2>
                   <div className="input-field">
                     <i className="fas fa-user"/>
-                    <input type="text" name="facultyname" value={this.state.facultyname} onChange={this.handlefacultychange}
-                           placeholder="Name"/>
+                    <input type="text" name="facultyname" value={this.state.facultyname} onChange={this.handlefacultychange} placeholder="Name"/>
                   </div>
+                  <div style={{color:"red",fontSize:"15px"}}>{this.state.nameerror}</div>
                   <div className="input-field">
                     <i className="fas fa-user"/>
-                    <input type="number" name="facultyphone" value={this.state.facultyphone} onChange={this.handlefacultychange}
-                           placeholder="Phone number"/>
+                    <input type="number" name="facultyphone" value={this.state.facultyphone} onChange={this.handlefacultychange} placeholder="Phone number"/>
                   </div>
+                  <div style={{color:"red",fontSize:"15px"}}>{this.state.phoneerror}</div>
                   <div className="input-field">
                     <i className="fas fa-envelope"/>
-                    <input type="email" name="facultyemail" value={this.state.facultyemail} onChange={this.handlefacultychange}
-                           placeholder="Email"/>
+                    <input type="email" name="facultyemail" value={this.state.facultyemail} onChange={this.handlefacultychange} placeholder="Email"/>
                   </div>
+                  <div style={{color:"red",fontSize:"15px"}}>{this.state.emailerror}</div>
                   <div className="input-field">
                     <i className="fas fa-lock"/>
-                    <input  type="password" name="facultypassword" value={this.state.facultypassword}
-                            onChange={this.handlefacultychange} placeholder="Password"/>
+                    <input  type="password" name="facultypassword" value={this.state.facultypassword} onChange={this.handlefacultychange} placeholder="Password"/>
                   </div>
+                  <div style={{color:"red",fontSize:"15px"}}>{this.state.passworderror}</div>
                   <Button style={{width:"100%",maxWidth:"385px",borderRadius:"55px",height:"45px"}} outline color="info" onClick={this.facultysubmit} >Register</Button>
-
                   <p style={{marginBottom:0}} className="social-text">Or Sign up with social platforms</p>
                     <div style={{display: "flex"}} className="social">
                         <a href="https://www.facebook.com/GZLearnings/">
-                            <img src="https://img.icons8.com/color/48/000000/facebook.png"/>
+                            <img src="https://img.icons8.com/color/48/000000/facebook.png" alt="social-icon"/>
                         </a>
                         <a href="https://www.instagram.com/groundzerolearnings/" >
-                          <img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/>
+                          <img src="https://img.icons8.com/fluent/48/000000/instagram-new.png" alt="social-icon"/>
                         </a>
                         <a href="https://g.page/groundzero-learnings/review?rc">
-                            <img src="https://img.icons8.com/color/48/000000/google-plus-squared.png"/>
+                            <img src="https://img.icons8.com/color/48/000000/google-plus-squared.png" alt="social-icon"/>
                         </a>
-                        <a href="https://www.linkedin.com/company/groundzero-learnings"
-                        >
-                            <img src="https://img.icons8.com/color/48/000000/linkedin.png"/>
+                        <a href="https://www.linkedin.com/company/groundzero-learnings">
+                            <img src="https://img.icons8.com/color/48/000000/linkedin.png" alt="social-icon"/>
                         </a>
                     </div>
                 </form>
@@ -260,7 +339,6 @@ class Auth  extends React.Component {
                     Sign up
                   </button>
                 </div>
-                <img src="/" className="image" alt=""/>
               </div>
               <div className="panel right-panel">
                 <div className="content">
@@ -276,7 +354,6 @@ class Auth  extends React.Component {
                     Sign up
                   </button>
                 </div>
-                {/*<img src={img} className="image" alt=""/>*/}
               </div>
             </div>
           </div>
