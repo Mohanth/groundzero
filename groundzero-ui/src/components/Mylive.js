65805abd09c2ff696e3db78023ac324db6d65c27
saisrivenkat <50038114+saisrivenkat@users.noreply.github.com>
2020-11-17 22:26:42 +0530

    Added css for Mylive

--- groundzero-ui/src/components/Mylive.js
+++ groundzero-ui/src/components/Mylive.js
@@ -15,18 +15,20 @@ class Live extends React.Component {
                     className="list-group-item  d-flex justify-content-between align-items-center panel-title ListItem collapsed" id="list-home-list"
                     data-toggle="collapse"
                     href={'#collapse' + courses.courseId} >
-                    <i class="fa fa-fw fa-chevron-down" />
-                  <i class="fa fa-fw fa-chevron-right" /><span>{courses.courseName}</span>
-                    <span className="badge badge-primary badge-pill">inprogress</span>
+                  {courses.courseName}
+                    <span className="badge badge-primary badge-pill">inprogress
+                    </span>
+                  <i className="fa fa-fw fa-chevron-down"/>
+                  <i className="fa fa-fw fa-chevron-left"/>
                 </li>
               </div>
               <div id={'collapse' + courses.courseId} className="panel-collapse collapse">
-                <ul className="list-group" style={{textAlign:'justify'}}>
-                  <li className="list-group-item" style={{backgroundColor: "#dee2e6 ",border: "none"}}>Cras justo odio</li>
-                  <li className="list-group-item" style={{backgroundColor: "#dee2e6 ",border: "none"}}>Dapibus ac facilisis in</li>
-                  <li className="list-group-item" style={{backgroundColor: "#dee2e6 ",border: "none"}}>Morbi leo risus</li>
-                  <li className="list-group-item" style={{backgroundColor: "#dee2e6 ",border: "none"}}>Porta ac consectetur ac</li>
-                  <li className="list-group-item" style={{backgroundColor: "#dee2e6 ",border: "none"}}>Vestibulum at eros</li>
+                <ul className="list-group content " style={{textAlign:'justify'}}>
+                  <li className="list-group-item" ><svg aria-hidden="true" focusable="false" className="svg-icon svg-xsmall video-icon" ><use xlinkHref="#icon-play"><svg id="icon-play" viewBox="0 0 24 24"><path opacity="0.9" d="M 2 12 c 0 5.525 4.475 10 10 10 s 10 -4.475 10 -10 S 17.525 2 12 2 S 2 6.475 2 12 Z m 15 0.114 L 9 16 V 8 l 8 4.114 Z" /></svg></use></svg><span>Cras justo odio</span></li>
+                  <li className="list-group-item" ><svg aria-hidden="true" focusable="false" className="svg-icon svg-xsmall video-icon" ><use xlinkHref="#icon-play"><svg id="icon-play" viewBox="0 0 24 24"><path opacity="0.9" d="M 2 12 c 0 5.525 4.475 10 10 10 s 10 -4.475 10 -10 S 17.525 2 12 2 S 2 6.475 2 12 Z m 15 0.114 L 9 16 V 8 l 8 4.114 Z" /></svg></use></svg><span> Dapibus ac facilisis in</span></li>
+                  <li className="list-group-item" ><svg aria-hidden="true" focusable="false" className="svg-icon svg-xsmall video-icon" ><use xlinkHref="#icon-play"><svg id="icon-play" viewBox="0 0 24 24"><path opacity="0.9" d="M 2 12 c 0 5.525 4.475 10 10 10 s 10 -4.475 10 -10 S 17.525 2 12 2 S 2 6.475 2 12 Z m 15 0.114 L 9 16 V 8 l 8 4.114 Z" /></svg></use></svg> <span>Morbi leo risus</span></li>
+                  <li className="list-group-item" ><svg aria-hidden="true" focusable="false" className="svg-icon svg-xsmall video-icon" ><use xlinkHref="#icon-play"><svg id="icon-play" viewBox="0 0 24 24"><path opacity="0.9" d="M 2 12 c 0 5.525 4.475 10 10 10 s 10 -4.475 10 -10 S 17.525 2 12 2 S 2 6.475 2 12 Z m 15 0.114 L 9 16 V 8 l 8 4.114 Z" /></svg></use></svg><span> Porta ac consectetur ac</span></li>
+                  <li className="list-group-item" ><svg aria-hidden="true" focusable="false" className="svg-icon svg-xsmall video-icon" ><use xlinkHref="#icon-play"><svg id="icon-play" viewBox="0 0 24 24"><path opacity="0.9" d="M 2 12 c 0 5.525 4.475 10 10 10 s 10 -4.475 10 -10 S 17.525 2 12 2 S 2 6.475 2 12 Z m 15 0.114 L 9 16 V 8 l 8 4.114 Z" /></svg></use></svg> <span>Vest ibulum at eros</span></li>
                 </ul>
               </div>
             </div>
